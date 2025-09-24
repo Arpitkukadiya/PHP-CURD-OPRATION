3af61cc103d03cd324a5f495c38316a732af220a
Kukadiya arpit <152589706+Arpitkukadiya@users.noreply.github.com>
2025-09-24 09:26:26 +0530

    Update visiterpage.php

--- visiterpage.php
+++ visiterpage.php
@@ -35,7 +35,7 @@ $visit=$_SESSION['visit'];
     <title>Document</title>
 </head>
 <body>
-    <h1>welcom</h1>
+    <h1>welcom to the first page of website</h1>
     <?php 
       if($email)
       {

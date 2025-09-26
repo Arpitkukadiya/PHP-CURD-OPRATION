83ea1e70387abd5cee36d37c451bf4b7c20c7882
Kukadiya arpit <152589706+Arpitkukadiya@users.noreply.github.com>
2025-09-26 17:42:23 +0530

    Update visiterpage.php

--- visiterpage.php
+++ visiterpage.php
@@ -35,7 +35,7 @@ $visit=$_SESSION['visit'];
     <title>Document</title>
 </head>
 <body>
-    <h1>welcom to website of ldrp student</h1>
+    <h1>welcom to website of ldrp student 1</h1>
     <?php 
       if($email)
       {

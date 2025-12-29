2bf54cb9da7206292bfd2f37142a13e781bc4010
Kukadiya arpit <152589706+Arpitkukadiya@users.noreply.github.com>
2025-12-29 17:46:55 +0530

    Update insert.php

--- insert.php
+++ insert.php
@@ -24,7 +24,7 @@ if($result){
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
+    <title>Document.</title>
 </head>
 <body>
     <form  method="POST">
